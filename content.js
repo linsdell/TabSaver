6805d9e30b079a8017a15184c86dae1c57332d19
Jamie Linsdell <jamie.linsdell@gmail.com>
2018-03-08 22:46:26 -0500

    starting to added functionality for storage
    working

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
+class tabGroup{
+  constructor(tabList, name){
+    this.tabList = tabList;
+    this.name = name;
+  }
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
@@ -21,10 +28,15 @@ window.onload = function () {
 $(document).ready(function(){
     $("#groupButton").click(function(){
     var list = $(".tabSelect");
+    var selectedList = [];
      for(i =0; i<list.length; i++){
         if(list[i].checked){
-          document.getElementById('savedTabs').innerHTML += '<p> added ' +  list[i].checked + " " + i + ' </p>';
+          selectedList.push(list[i].value);
+        //  document.getElementById('savedTabs').innerHTML += '<p> added ' +  list[i].checked + " " + i + ' </p>';
        }
+    }
+    for(i = 0; i<selectedList.length;i++){
+      document.getElementById('savedTabs').innerHTML += '<p>' +  selectedList[i] + ' </p>';
     }
     });
 });
